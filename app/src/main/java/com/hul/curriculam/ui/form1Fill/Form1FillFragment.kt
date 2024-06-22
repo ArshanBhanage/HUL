c7eb6cc7cdd8e28ddf93de8fff619136806197f2
nitin <nitin.chorge@mintwalk.com>
2024-06-22 12:01:36 +0530

    added sync code

--- app/src/main/java/com/hul/curriculam/ui/form1Fill/Form1FillFragment.kt
+++ app/src/main/java/com/hul/curriculam/ui/form1Fill/Form1FillFragment.kt
@@ -150,10 +150,15 @@ class Form1FillFragment : Fragment(), ApiHandler, RetryInterface {
 
         binding.proceed.setOnClickListener {
             if (imageIndex == 0) {
-                setProgressDialog(requireContext(), "Uploading")
-                uploadImage(form1FillViewModel.imageUrl1.value?.toUri()!!)
-//                val visitDataTable = VisitDataTable(jsonData= Gson().toJson(submitModel()), visitNumber = form1FillViewModel.projectInfo.value!!.visit_number!!.toInt(),locationName = form1FillViewModel.projectInfo.value!!.location_name!!, uDiceCode = form1FillViewModel.selectedSchoolCode.value!!.external_id1!!)
-//                visitDataViewModel.insert(visitDataTable)
+//                setProgressDialog(requireContext(), "Uploading")
+//                uploadImage(form1FillViewModel.imageUrl1.value?.toUri()!!)
+                val visitDataTable = VisitDataTable(jsonData= Gson().toJson(submitModel()), visitNumber = form1FillViewModel.projectInfo.value!!.visit_number!!.toInt(),locationName = form1FillViewModel.projectInfo.value!!.location_name!!, uDiceCode = form1FillViewModel.selectedSchoolCode.value!!.external_id1!!)
+                visitDataViewModel.insert(visitDataTable)
+
+                val intent = Intent(activity, Dashboard::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                requireActivity().finish()
             }
         }
 
@@ -571,10 +576,10 @@ class Form1FillFragment : Fragment(), ApiHandler, RetryInterface {
                 number_of_books_distributed = VisitDetails(value = form1FillViewModel.noOfBooksHandedOver.value),
                 no_of_teachers_trained = VisitDetails(value = form1FillViewModel.teachersTrained.value),
 
-                visit_image_1 = VisitDetails(value = form1FillViewModel.imageApiUrl1.value),
-                visit_image_2 = VisitDetails(value = form1FillViewModel.imageApiUrl2.value),
-                visit_image_3 = VisitDetails(value = form1FillViewModel.imageApiUrl3.value),
-                visit_image_4 = VisitDetails(value = form1FillViewModel.imageApiUrl4.value),
+                visit_image_1 = VisitDetails(value = form1FillViewModel.imageUrl1.value),
+                visit_image_2 = VisitDetails(value = form1FillViewModel.imageUrl2.value),
+                visit_image_3 = VisitDetails(value = form1FillViewModel.imageUrl3.value),
+                visit_image_4 = VisitDetails(value = form1FillViewModel.imageUrl4.value),
 
                 name_of_the_school_representative_who_collected_the_books = VisitDetails(value = form1FillViewModel.form1.value.toString()),
                 mobile_number_of_the_school_representative_who_collected_the_books = VisitDetails(
@@ -746,7 +751,7 @@ class Form1FillFragment : Fragment(), ApiHandler, RetryInterface {
         isTimerStarted = true
         //binding.proceed.isEnabled = false
 
-        val totalTime = 20 * 60 * 1000L
+        val totalTime = 1 * 60 * 1000L
 
         // Set initial time before starting the timer
         updateTimerText(totalTime)
--- app/src/main/java/com/hul/curriculam/ui/form3Fill/Form3FillFragment.kt
+++ app/src/main/java/com/hul/curriculam/ui/form3Fill/Form3FillFragment.kt
@@ -50,6 +50,8 @@ import com.hul.data.VisitData
 import com.hul.data.VisitDetails
 import com.hul.databinding.FragmentForm3FillBinding
 import com.hul.screens.field_auditor_dashboard.ui.image_preview.ImagePreviewDialogFragment
+import com.hul.sync.VisitDataTable
+import com.hul.sync.VisitDataViewModel
 import com.hul.user.UserInfo
 import com.hul.utils.ConnectionDetector
 import com.hul.utils.RetryInterface
@@ -84,6 +86,9 @@ class Form3FillFragment : Fragment(), ApiHandler, RetryInterface {
 
     @Inject
     lateinit var uploadFileController: UploadFileController
+
+    @Inject
+    lateinit var visitDataViewModel: VisitDataViewModel
 
     var imageIndex: Int = 0
 
@@ -162,10 +167,22 @@ class Form3FillFragment : Fragment(), ApiHandler, RetryInterface {
 
         binding.proceed.setOnClickListener {
             if (imageIndex == 0) {
-                setProgressDialog(requireContext(), "Uploading")
-                uploadImage(form3FillViewModel.imageUrl1.value?.toUri()!!)
+                val visitDataTable = VisitDataTable(jsonData= Gson().toJson(submitModel()), visitNumber = form3FillViewModel.projectInfo.value!!.visit_number!!.toInt(),locationName = form3FillViewModel.projectInfo.value!!.location_name!!, uDiceCode = form3FillViewModel.selectedSchoolCode.value!!.external_id1!!)
+                visitDataViewModel.insert(visitDataTable)
+
+                val intent = Intent(activity, Dashboard::class.java)
+                intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+                startActivity(intent)
+                requireActivity().finish()
             }
         }
+
+//        binding.proceed.setOnClickListener {
+//            if (imageIndex == 0) {
+//                setProgressDialog(requireContext(), "Uploading")
+//                uploadImage(form3FillViewModel.imageUrl1.value?.toUri()!!)
+//            }
+//        }
 
         binding.view1.setOnClickListener {
             form3FillViewModel.imageUrl1.value?.let { it1 ->
